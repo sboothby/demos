95db3279123446a31892121544930ee89afae54f
Stephen Boothby <sboothby510@gmail.com>
2021-07-27 10:41:03 -0400

    add REST controller, REST exception handlers, DI bean setup

--- car-rental-api/src/main/java/com/boothby/car/rental/api/config/ConfigBeans.java
+++ car-rental-api/src/main/java/com/boothby/car/rental/api/config/ConfigBeans.java
@@ -1,8 +1,49 @@
 package com.boothby.car.rental.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.boothby.car.rental.api.core.CarLocatorService;
+import com.boothby.car.rental.api.core.DriverHistoryService;
+import com.boothby.car.rental.api.core.InsuranceBinderService;
+import com.boothby.car.rental.api.core.RentalAgreementService;
+import com.boothby.car.rental.api.core.RentalService;
+import com.boothby.car.rental.api.core.impl.DefaultCarLocatorService;
+import com.boothby.car.rental.api.core.impl.DefaultDriverHistoryService;
+import com.boothby.car.rental.api.core.impl.DefaultInsuranceBinderService;
+import com.boothby.car.rental.api.core.impl.DefaultRentalAgreementService;
+import com.boothby.car.rental.api.core.impl.DefaultRentalService;
+
+/**
+ * Performs Spring DI of beans.  Does any custom bean setup.  Loosely-coupled shared beans are centrally initialized
+ * here through Spring.
+ */
 @Configuration
 public class ConfigBeans {
 
+    @Bean
+    public DriverHistoryService driverHistoryService() {
+        return new DefaultDriverHistoryService();
+    }
+    
+    @Bean
+    public CarLocatorService carLocatorService() {
+        return new DefaultCarLocatorService();
+    }
+    
+    @Bean
+    public InsuranceBinderService insuranceBinderService() {
+        return new DefaultInsuranceBinderService();
+    }
+    
+    @Bean
+    public RentalAgreementService rentalAgreementService() {
+        return new DefaultRentalAgreementService();
+    }
+    
+    @Bean
+    public RentalService rentalService(DriverHistoryService driverHistoryService, CarLocatorService carLocatorService,
+            InsuranceBinderService insuranceBinderService, RentalAgreementService rentalAgreementService) {
+        return new DefaultRentalService(driverHistoryService, carLocatorService, insuranceBinderService, rentalAgreementService);
+    }
 }
--- car-rental-api/src/main/java/com/boothby/car/rental/api/drivers/rest/controllers/RentalController.java
+++ car-rental-api/src/main/java/com/boothby/car/rental/api/drivers/rest/controllers/RentalController.java
@@ -1,8 +1,63 @@
 package com.boothby.car.rental.api.drivers.rest.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.boothby.car.rental.api.core.RentalService;
+import com.boothby.car.rental.api.core.exception.CarNotFoundException;
+import com.boothby.car.rental.api.core.exception.DriverException;
+import com.boothby.car.rental.api.core.exception.InsuranceException;
+import com.boothby.car.rental.api.core.exception.RentalContractException;
+import com.boothby.car.rental.api.core.exception.RentalValidationException;
+import com.boothby.car.rental.api.core.model.RentalCarRequest;
+import com.boothby.car.rental.api.core.model.RentalContract;
 
 @RestController
 public class RentalController {
 
+    private RentalService rentalService;
+
+    @Autowired
+    public RentalController(RentalService rentalService) {
+        this.rentalService = rentalService;
+    }
+
+    @PostMapping("/rental-cars")
+    public ResponseEntity<RentalContract> rentCar(@RequestBody RentalCarRequest rentalCarRequest)
+            throws RentalValidationException, DriverException, CarNotFoundException, InsuranceException,
+            RentalContractException {
+        // Create new rental car reservation.
+        RentalContract rentalContract = rentalService.rentCar(rentalCarRequest);
+        return new ResponseEntity<RentalContract>(rentalContract, HttpStatus.CREATED);
+    }
+    
+    @ExceptionHandler(RentalValidationException.class)
+    public final ResponseEntity<String> handleRentalValidationException(RentalValidationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RentalContractException.class)
+    public final ResponseEntity<String> handleRentalContractException(RentalContractException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
+    }
+    
+    @ExceptionHandler(DriverException.class)
+    public final ResponseEntity<String> handleInternalServiceException(DriverException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler(InsuranceException.class)
+    public final ResponseEntity<String> handleInternalServiceException(InsuranceException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler(RentalContractException.class)
+    public final ResponseEntity<String> handleInternalServiceException(RentalContractException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
+    }
 }
